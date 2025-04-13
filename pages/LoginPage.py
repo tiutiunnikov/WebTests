b56f2e456ce5358a855218b64113c4377314728f
Konstantin Tiutiunnikov <konstantin.tiutiunnikov@gmail.com>
2025-04-13 11:20:52 +0300

    Allure for tests

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -1,3 +1,5 @@
+import allure
+
 from pages.BasePage import BasePage
 from selenium.webdriver.common.by import By
 
@@ -34,11 +36,14 @@ class LoginPageHelper(BasePage):
         self.find_element(LoginPageLocators.MAILRU_BUTTON)
         self.find_element(LoginPageLocators.YANDEX_BUTTON)
 
-
+    @allure.step('Нажимаем на кнопку "Войти')
     def click_login(self):
+        self.attach_screenshot()
         self.find_element(LoginPageLocators.LOGIN_BUTTON).click()
 
+    @allure.step('Получаем текст ошибки')
     def get_error_text(self):
+        self.attach_screenshot()
         return self.find_element(LoginPageLocators.ERROR_TEXT).text
 
     def send_keys(self):
--- tests/LoginTests.py
+++ tests/LoginTests.py
@@ -1,3 +1,5 @@
+import allure
+
 from core.BaseTest import browser
 from pages.BasePage import BasePage
 from pages.LoginPage import LoginPageHelper
@@ -6,12 +8,16 @@ BASE_URL = 'https://ok.ru/'
 EMPTY_LOGIN_ERROR = 'Enter your username'
 EMPTY_PASSWORD_ERROR = 'Enter password'
 
+@allure.suite('Проверка формы авторизации')
+@allure.title('Проверка ошибки при пустой форме авторизации')
 def test_empty_login_and_password(browser):
     BasePage(browser).get_url(BASE_URL)
     LoginPage = LoginPageHelper(browser)
     LoginPage.click_login()
     assert LoginPage.get_error_text() == EMPTY_LOGIN_ERROR
 
+@allure.suite('Проверка формы авторизации')
+@allure.title('Проверка ошибки при пустом поле "Пароль"')
 def test_empty_password(browser):
     BasePage(browser).get_url(BASE_URL)
     LoginPage = LoginPageHelper(browser)
